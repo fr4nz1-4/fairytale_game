63e66dbe47ad0ecc2ef946a11d022fbbecbd53c7
fr4nz1-4 <captain.proton.paris@gmail.com>
2025-04-26 15:24:38 +0200

    add character sprites, create first room and mouse click test

--- Assets/AdventureScript.cs
+++ Assets/AdventureScript.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Actionadventure : MonoBehaviour
+public class AdventureScript : MonoBehaviour
 {
     public Vector3 mousePosition;
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            print("left mouse click");
+            print("left mouse button clicked");
             mousePosition = Input.mousePosition;
             print(mousePosition);
         }
